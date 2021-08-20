726ad01a09f7e104ea4a2f281ed614258e8323bd
1Mill <erik.ekberg@outlook.com>
2021-08-19 22:08:58 -0700

    Scaffold invokes of other rapids-v0 lambdas

--- src/rapids-v0/hydrator/index.js
+++ src/rapids-v0/hydrator/index.js
@@ -1,11 +1,23 @@
 const { invoke } = require('@1mill/lambda')
 
+const config = {
+	accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+	endpoint: process.env.AWS_ENDPOINT,
+	region: process.env.AWS_REGION,
+	secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+}
+
 const perform = async () => {
-	console.log('Hello world!')
-	console.log('Hello world!')
-	console.log(invoke)
-	console.log('Hello world!')
-	console.log('Hello world!')
+	await Promise.allSettled([
+		invoke({
+			...config,
+			functionName: 'rapids-v0-journal',
+		}),
+		invoke({
+			...config,
+			functionName: 'rapids-v0-websockets',
+		}),
+	])
 }
 
 exports.handler = async (event, ctx) => await perform({ cloudevent: event, ctx })
