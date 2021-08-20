d0eb9368504d482f4b91e2ab573a87c339d1bd2c
1Mill <erik.ekberg@outlook.com>
2021-08-19 19:57:52 -0700

    Scaffold dev env vars

--- src/rapids-v0/hydrator/config.bash
+++ src/rapids-v0/hydrator/config.bash
@@ -4,6 +4,8 @@ FUNCTION_NAME=rapids-v0-hydrator
 TIMEOUT=3
 
 DEV_ENVIRONMENT="{
-	AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID,
-	AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY,
+	AWS_ACCESS_KEY_ID=string-exists-for-development,
+	AWS_ENDPOINT: http://localstack:4566/,
+	AWS_REGION=us-east-1,
+	AWS_SECRET_ACCESS_KEY=string-exists-for-development,
 }"
