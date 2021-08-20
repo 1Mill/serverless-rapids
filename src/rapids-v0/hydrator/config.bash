ff4f4acc713e3bbb79e546389e1bcc187a3435f9
1Mill <erik.ekberg@outlook.com>
2021-08-19 22:06:39 -0700

    Fix syntax

--- src/rapids-v0/hydrator/config.bash
+++ src/rapids-v0/hydrator/config.bash
@@ -5,7 +5,7 @@ TIMEOUT=3
 
 DEV_ENVIRONMENT="{
 	AWS_ACCESS_KEY_ID=string-exists-for-development,
-	AWS_ENDPOINT: http://localstack:4566/,
+	AWS_ENDPOINT=http://localstack:4566/,
 	AWS_REGION=us-east-1,
 	AWS_SECRET_ACCESS_KEY=string-exists-for-development,
 }"
