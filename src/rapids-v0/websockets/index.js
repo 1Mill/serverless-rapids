647537d6ae9fcbc8c0740da91c4e398f3ddc6fe3
1Mill <erik.ekberg@outlook.com>
2021-08-20 15:41:44 -0700

    Scaffold connection to ABLY

--- src/rapids-v0/websockets/index.js
+++ src/rapids-v0/websockets/index.js
@@ -1,34 +1,27 @@
-const Pusher = require('pusher')
+const Ably = require('ably/promises')
 
-const CHANNEL_DELIMITER = '::'
+const CHANNEL_DELIMITER = ':'
 const CHANNEL_NAMESPACE = 'rapids-v0'
-const CHANNLE_VERSION = '2021-08-19'
-
-const pusher = new Pusher({
-	appId: process.env.PUSHER_APPID,
-	cluster: process.env.PUSHER_CLUSTER,
-	key: process.env.PUSHER_KEY,
-	secret: process.env.PUSHER_SECRET,
-	useTLS: true,
-})
+const CHANNLE_VERSION = '2021-08-20'
 
 const perform = async ({ cloudevent, ctx }) => {
-	const { id, source, type } = cloudevent
+	const { id , source, type } = cloudevent
 
-	// ! Attribute order matters for clients to connect
-	// ! to the correct channel
-	// private::rapids-v0::2021-08-19::com.myreallylongwebsitename.mysubdomain::79959f93-d298-41b5-a299-e58a3e2fb82b
-	const channel = [
-		'private',
+	// ! Attribute order matters for clients to
+	// ! connect to the correct channel
+	// rapids-v0:2021-08-19:com.myreallylongwebsitename.mysubdomain:79959f93-d298-41b5-a299-e58a3e2fb82b
+	const channelName = [
 		CHANNEL_NAMESPACE,
 		CHANNLE_VERSION,
 		source,
 		id,
 	].join(CHANNEL_DELIMITER)
-	const message = type
-	const payload = { cloudevent }
 
-	await pusher.trigger(channel, message, payload)
+	// * REST library is stateless so we don't have to manage connection state
+	// * https://knowledge.ably.com/should-i-use-the-rest-or-realtime-library
+	const client = new Ably.Rest.Promise(process.env.ABLY_API_KEY)
+	const channel = client.channels.get(channelName)
+	await channel.publish(type, { cloudevent })
 }
 
-exports.handler = async (event, ctx) => await perform({ cloudevent: event || {}, ctx })
+exports.handler = async (cloudevent, ctx) => await perform({ cloudevent, ctx })
