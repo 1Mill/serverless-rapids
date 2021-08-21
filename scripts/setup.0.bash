d540ce54a89f3eee047166a5976626100875e51f
1Mill <erik.ekberg@outlook.com>
2021-08-20 19:46:18 -0700

    Refactor setup script to work for multiple files

--- scripts/setup.0.bash
+++ scripts/setup.0.bash
@@ -1,9 +1,23 @@
 #!/bin/bash
 
 function setup () {
-	source $(dirname $BASH_SOURCE)/build.0.bash &
-	source $(dirname $BASH_SOURCE)/publish.0.bash
-	wait
+	local DIR=$1
+
+	local dirs=$(
+		find $DIR \
+		-name config.bash \
+		-not -path *node_modules* \
+		-not -path *terraform* \
+		-type f \
+		-print0 | xargs -0 -n1 dirname
+	)
+
+	for dir in $dirs
+	do
+		source $(dirname $BASH_SOURCE)/build.0.bash $dir &
+		source $(dirname $BASH_SOURCE)/publish.0.bash $dir
+		wait
+	done
 }
 
 setup $1
