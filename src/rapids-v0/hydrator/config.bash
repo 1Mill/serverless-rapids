d227978705e9c92a7d80389eb94b03565171859c
1Mill <erik.ekberg@outlook.com>
2021-08-23 19:50:03 -0700

    Update config.bash

--- src/rapids-v0/hydrator/config.bash
+++ src/rapids-v0/hydrator/config.bash
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 FUNCTION_NAME=rapids-v0-hydrator
-TIMEOUT=3
+TIMEOUT=30
 
 DEV_ENVIRONMENT="{
-	AWS_ACCESS_KEY_ID=string-exists-for-development,
+	AWS_ACCESS_KEY_ID=test,
 	AWS_ENDPOINT=http://localstack:4566/,
 	AWS_REGION=us-east-1,
-	AWS_SECRET_ACCESS_KEY=string-exists-for-development,
+	AWS_SECRET_ACCESS_KEY=test,
 	NODE_ENV=development,
 }"
