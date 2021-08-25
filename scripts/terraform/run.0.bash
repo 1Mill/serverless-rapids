ad6134401aed04a86acc8c2ef0e4a6a538b95388
1Mill <erik.ekberg@outlook.com>
2021-08-24 19:07:35 -0700

    Pass through credentials from root .env file to terraform runtime

--- scripts/terraform/run.0.bash
+++ scripts/terraform/run.0.bash
@@ -10,9 +10,21 @@ function run() {
 	local CONFIG_FILE_PATH=$(pwd)/$DIR/$CONFIG_FILENAME
 	source $CONFIG_FILE_PATH
 
+	# * Check for .env file in project root for credentials
+	local ENV_FILE_PATH=$(dirname $BASH_SOURCE)/../../.env
+	if [ -f "$ENV_FILE_PATH" ]; then
+		source $ENV_FILE_PATH
+	fi
+
+	local AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
+	local AWS_DEFAULT_REGION=us-east-1
+	local AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
 	local FILE=$(dirname $BASH_SOURCE)/docker-compose.$VERSION.yml
 	local TERRAFORM_VERSION=$TERRAFORM_VERSION
 
+	AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
+	AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION \
+	AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
 	DIR=$(pwd)/$1 \
 	TERRAFORM_VERSION=$TERRAFORM_VERSION \
 	docker-compose -f $FILE run terraform $ACTION
