0d131a3e98a0fdc5216be0051da9ebd2dfae29a8
1Mill <erik.ekberg@outlook.com>
2021-08-31 17:01:35 -0700

    Setup journal integration so that events are (1) recorded and (2) no duplicates occure

--- src/rapids-v0/websockets/config.bash
+++ src/rapids-v0/websockets/config.bash
@@ -6,5 +6,8 @@ VERSION=2021-08-24T07-51-00
 
 DEV_ENVIRONMENT="{
 	ABLY_API_KEY=$ABLY_API_KEY,
+	JOURNAL_DB_NAME=development,
+	JOURNAL_DB_URI=mongodb://root:password@mongo-able:27017/,
+	JOURNAL_TABLE_NAME=journal.$FUNCTION_NAME.v0,
 	NODE_ENV=development,
 }"
--- src/rapids-v0/websockets/index.js
+++ src/rapids-v0/websockets/index.js
@@ -1,16 +1,29 @@
 const Ably = require('ably/promises')
+const { Journal } = require('@1mill/journal')
 
 const CHANNEL_NAME = 'rapids-v0:2021-08-20'
 
+const journal = new Journal({})
+
 const perform = async ({ cloudevent, ctx }) => {
+	ctx.callbackWaitsForEmptyEventLoop = false
 
-	if (!cloudevent.type) throw new Error('[contract] cloudevent.type is required')
+	try {
+		const { skip } = await journal.entry({ cloudevent })
+		if (skip) return
 
-	// * REST library is stateless so we don't have to manage connection state
-	// * https://knowledge.ably.com/should-i-use-the-rest-or-realtime-library
-	const client = new Ably.Rest.Promise(process.env.ABLY_API_KEY)
-	const channel = client.channels.get(CHANNEL_NAME)
-	await channel.publish(cloudevent.type, { cloudevent })
+		if (!cloudevent.type) throw new Error('[contract] cloudevent.type is required')
+
+		// * REST library is stateless so we don't have to manage connection state
+		// * https://knowledge.ably.com/should-i-use-the-rest-or-realtime-library
+		const client = new Ably.Rest.Promise(process.env.ABLY_API_KEY)
+		const channel = client.channels.get(CHANNEL_NAME)
+		await channel.publish(cloudevent.type, { cloudevent })
+	} catch (err) {
+		console.error(err)
+		await journal.erase({ cloudevent })
+		throw new Error(err)
+	}
 }
 
 exports.handler = async (cloudevent = {}, ctx = {}) => await perform({ cloudevent, ctx })
