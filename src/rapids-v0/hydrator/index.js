aaccbc8b74966efeff88fd5da6aecaf8f0cf6957
1Mill <erik.ekberg@outlook.com>
2021-09-04 10:44:05 -0700

    Use Lambda library

--- src/rapids-v0/hydrator/index.js
+++ src/rapids-v0/hydrator/index.js
@@ -1,25 +1,11 @@
-const { LambdaClient, InvokeCommand } = require('@aws-sdk/client-lambda')
+const { Lambda } = require('@1mill/lambda')
 const { getFunctionIds } = require('./getFunctionIds')
-const { v6: { createCloudevent } } = require('@1mill/cloudevents')
 
-let aws
-if (typeof aws === 'undefined') {
-	aws = new LambdaClient({
-		credentials: {
-			accessKeyId: process.env.INVOKE_AWS_ACCESS_KEY_ID,
-			secretAccessKey: process.env.INVOKE_AWS_SECRET_ACCESS_KEY,
-		},
-		endpoint: process.env.INVOKE_AWS_ENDPOINT,
-		maxAttempts: 3,
-		region: process.env.INVOKE_AWS_REGION,
-	})
-}
+const lambda = new Lambda({})
 
 exports.handler = async (cloudevent = {}, ctx = {}) => {
 	// * https://www.jeremydaly.com/reuse-database-connections-aws-lambda/
 	ctx.callbackWaitsForEmptyEventLoop = false
-
-	createCloudevent({ ...cloudevent }) // * Validate attributes of cloudevent
 
 	const functionIds = [
 		'rapids-v0-websockets',
@@ -28,12 +14,11 @@ exports.handler = async (cloudevent = {}, ctx = {}) => {
 			type: cloudevent.type,
 		}),
 	]
-	const commands = functionIds.map(id => new InvokeCommand({
-		FunctionName: id,
-		InvocationType: 'Event',
-		Payload: JSON.stringify(cloudevent),
+
+	const promises = functionIds.map(async id => await lambda.invoke({
+		cloudevent,
+		functionName: id,
 	}))
-	const promises = commands.map(async c => await aws.send(c))
 
 	const responses = await Promise.allSettled(promises)
 	responses
